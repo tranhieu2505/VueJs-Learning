2ea3b6221b800e4a2d791ca70a0d106ce18c913a
tranhieu2505 <tranhieuhus@gmail.com>
2024-10-11 10:49:41 +0700

    upload library nodejs in WPVuejs

--- main.js
+++ main.js
@@ -7,27 +7,48 @@ var vueInstance = new Vue({
         price: 20000,
         sale: 0.1,
         selectedProduct: 1,
+        cartNumber: 1,
         listProductDetail: [
             {
                 image: './images/blue.jpg',
-                quanlity: 8,
+                quantity: 8,
                 textColor: 'Blue'
             },
             {
                 image: './images/black.jpg',
-                quanlity: 10,
+                quantity: 10,
                 textColor: 'Black'
             },
             {
                 image: './images/red.jpg',
-                quanlity: 0,
+                quantity: 0,
                 textColor: 'Red'
             },
-        ]
+        ],
+        listDesc: [
+            'Chất liệu: polyester và thun',
+            'Thoát mồ hôi tốt',
+            'Áo thun cổ tròn thể thao Hiye chuyên được may từ chất liệu nilon thoáng mát',
+            'Kết hợp thêm sợi thun tạo độ co giãn giúp người tiêu dùng thoải mái khi mặc',
+            'Chất liệu: polyester và thun'
+        ],
+        description: '<p>M&ocirc; tả: &Aacute;o thun cổ tr&ograve;n thể thao Hiye chuy&ecirc;n được may từ chất liệu nilon tho&aacute;ng m&aacute;t v&agrave; tho&aacute;t mồ h&ocirc;i tốt, kết hợp th&ecirc;m sợi thun tạo độ co gi&atilde;n gi&uacute;p người ti&ecirc;u d&ugrave;ng thoải m&aacute;i khi mặc</p> ',
+        testDescription: `<p>Test hjskhfk h</p>
+
+        <p><strong><span style="background-color:#8e44ad">fhsdjkfhsk&nbsp; hsdjkfk&nbsp;</span></strong></p>`
+
     },
+
     methods:{
         handleClickColor(e, index) {
             this.selectedProduct = index;
+        },
+        handleAddToCart(e) {
+            if(this.cartNumber + 1 > this.getProduct.quantity) {
+                alert('Quantity is not enough.')
+            } else {
+                this.cartNumber = this.cartNumber  + 1;
+            }
         }
     },
     computed: {
